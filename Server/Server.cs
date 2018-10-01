c036efb46959cb568e9d6dc1ceb34e225c203fad
Gerben van Dooren <pedooren@avans.nl>
2018-10-01 13:01:31 +0200

    [Added] User class, modified

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SharedData;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,39 @@ namespace Server
 {
     class Server
     {
+        public static List<Car> cars { get; set; }
+        public static List<User> users { get; set; }
+
+        private static TcpListener server;
+
+        public void Main(string[] args)
+        {
+            server = new TcpListener(10000);
+            server.Start();
+            server.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+
+        }
+
+        private static void OnConnect(IAsyncResult ar)
+        {
+            TcpClient client = server.EndAcceptTcpClient(ar);
+            users.Add(new User(client));
+
+            server.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+        }
+
+        public static void Broadcast()
+        {
+            foreach (User user in users)
+            {
+                //Do statement
+            }
+        }
+
+        public static void BroadcastExcept(User user, dynamic data)
+        {
+            foreach (User u in users.Where(u => u != user))
+                u.Send(data);
+        }
     }
 }
---
+++ Server/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class User
+    {
+        private byte[] buffer = new byte[1024];
+        private string totalBuffer = "";
+        public TcpClient client { get; set; }
+        public string username { get; set; }
+
+        public User(TcpClient client)
+        {
+            this.client = client;
+            client.GetStream().BeginRead(buffer, 0, 1024, new AsyncCallback(OnRead), this);
+        }
+
+
+
+        private void OnRead(IAsyncResult ar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(String data)
+        {
+            byte[] byteData = Encoding.UTF8.GetBytes(data);
+            client.GetStream().Write(byteData, 0, byteData.Length);
+        }
+    }
+}
