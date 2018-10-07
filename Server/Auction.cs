e5c5ab6a62df9d610f0a402f43dbd2a02ca1bcb9
Gerben van Dooren <pedooren@avans.nl>
2018-10-07 12:05:19 +0200

    [Created] Class Auction
    ToString methods split:
    1) ClientsToString
    2) BidsToString
    3)  1 + 2

---
+++ Server/Auction.cs
@@ -0,0 +1,70 @@
+﻿using SharedData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Auction
+    {
+        public const string SAVELOCATION = @"SavedAuctions\";
+
+        public Car Car { get; private set; }
+        public int AuctionID { get; private set; }
+        public List<Client> Clients { get; private set; }
+        public Client SuccessfullBidder { get; private set; } = null;
+        public int AuctionTime { get; set; }
+        public DateTime StartTime { get; private set; }
+        public bool Running { get; private set; }
+
+
+        public Auction(List<Client> clients, Car car)
+        {
+            Clients = clients;
+            Car = car;
+            AuctionID = car.CarID;
+        }
+
+        public void StartAuction(int auctionTime)
+        {
+            Car.bids.Clear();
+            AuctionTime = auctionTime;
+            Running = true;
+            StartTime = DateTime.Now;
+        }
+
+        public string ClientsToString(List<Client>clients)
+        {
+            string ClientList = "Clients in this Room:";
+            foreach (Client c in clients)
+            {
+                ClientList += $"\r\n- Client: {c.Socket.Client.LocalEndPoint}";
+            }
+            return ClientList;
+        }
+
+        public string BidsToString(Car car)
+        {
+            string Bids = $"This are all the bids on Car: {car.Brand}, with ID: {car.CarID}:";
+            foreach(Bid b in car.bids)
+            {
+                Bids += $"\n\rUser: blablabla is bidding: {b.Amount} on: {b.Time}.";
+            }
+            Bids += $"\n\rSuccessfull Bidder: ";
+            return Bids;
+        }
+
+        public string ToString(List<Client> clients, Car car)
+        {
+            string Auction = "";
+            Auction += $"Auction number: {AuctionID}, AuctionTime: {AuctionTime}, Running: {Running}\n\r";
+            Auction += $"Clients: {clients.Count()}\n\r\n\r";
+            Auction += BidsToString(car);
+
+            return Auction;
+        }
+
+    }
+}
